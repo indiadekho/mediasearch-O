866dc4a5930c45c5175213511fa56c750b5a525e
insaneman22 <76441053+insaneman22@users.noreply.github.com>
2021-04-11 20:55:10 +0530

    Update forward.py

--- plugins/forward.py
+++ plugins/forward.py
@@ -9,9 +9,13 @@ logger = logging.getLogger(__name__)
 lock = asyncio.Lock()
 PRIVATE_CHANNEL_ID = -10012345677
 
-@Client.on_message(filters.command('forward') & filters.user(ADMINS) & (filters.video | filters.document))
+@Client.on_message(filters.command('forward') & filters.user(ADMINS))
 async def index_files(bot, message):
     """Save channel or group files with the help of user bot"""
+    
+@Client.on_message(filters.video & filters.document))
+async def index_files(bot, message)
+
     if not USERBOT_STRING_SESSION:
         await message.reply('Set `USERBOT_STRING_SESSION` in info.py file or in environment variables.')
     elif len(message.command) == 1:
