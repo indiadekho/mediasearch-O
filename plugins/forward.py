cbe114b5185eeb1d3b768fa196ba45e9df6b3fcf
insaneman22 <76441053+insaneman22@users.noreply.github.com>
2021-06-03 12:19:51 +0530

    Update forward.py

--- plugins/forward.py
+++ plugins/forward.py
@@ -32,7 +32,7 @@ async def index_files(bot, message):
                     for chat in chats:
                         
                         async for message in user_bot.iter_history(chat):
-                            if not message.video:
+                            if not message.document:
                              continue
                             
                             try:
